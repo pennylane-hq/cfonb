959d9482d96c1b2a7da05cd64e2546e9be5ac778
Johan Le Bray <15625131+jlebray@users.noreply.github.com>
2023-09-26 09:38:36 +0200

    Fix: Remove empty references

--- lib/cfonb/operation_detail/rcn.rb
+++ lib/cfonb/operation_detail/rcn.rb
@@ -3,13 +3,15 @@
 module CFONB
   module OperationDetail
     class RCN
+      using CFONB::Refinements::Strings
+
       ATTRIBUTES = %i[reference purpose].freeze
 
       def self.apply(operation, line)
         operation.reference = [
           operation.reference,
-          line.detail[0..34].strip
-        ].compact.join(' - ')
+          line.detail[0..34].strip,
+        ].filter_map(&:presence).join(' - ')
 
         operation.purpose = line.detail[35..-1]&.strip
       end
--- lib/cfonb/refinements/strings.rb
+++ lib/cfonb/refinements/strings.rb
@@ -4,13 +4,16 @@ module CFONB
   module Refinements
     module Strings
       refine String do
-
         def first(count = 1)
           self[..(count - 1)]
         end
 
         def last(count = 1)
           self[-count..]
+        end
+
+        def presence
+          strip.empty? ? nil : self
         end
       end
     end
