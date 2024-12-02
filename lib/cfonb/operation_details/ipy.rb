6535df552584ceab2f865b248152b06c0773882e
Laura Gil <laura.gil@pennylane.com>
2024-12-02 17:39:02 +0100

    Add safe operator (#27)

--- lib/cfonb/operation_details/ipy.rb
+++ lib/cfonb/operation_details/ipy.rb
@@ -7,7 +7,7 @@ module CFONB
 
       def self.apply(details, line)
         details.debtor_identifier = line.detail[0..34].strip
-        details.debtor_identifier_type = line.detail[35..-1].strip
+        details.debtor_identifier_type = line.detail[35..-1]&.strip
       end
 
       CFONB::OperationDetails.register('IPY', self)
